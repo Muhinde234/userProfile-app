2042bc3242eccc82fa054a6110ef3aaa4d3508e9
ICYEZAGATORE <i.icyezagat1@alustudent.com>
2025-03-25 14:29:45 +0200

    Question 1

---
+++ assets/pages/playground.js
@@ -0,0 +1,9 @@
+let username= "Jane Doe";
+const birthYear= 1980;
+const currentYear= 2025;
+function calculateAge(){
+let age = currentYear-birthYear;
+console.log(`Hello your name is ${username} and your age is${age}`); 
+}
+
+calculateAge();
