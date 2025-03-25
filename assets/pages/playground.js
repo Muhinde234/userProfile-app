f783305b7b7f7198d6f7557f5780f996d9fada28
ICYEZAGATORE <i.icyezagat1@alustudent.com>
2025-03-25 14:37:10 +0200

    Question 4

--- assets/pages/playground.js
+++ assets/pages/playground.js
@@ -7,3 +7,9 @@ console.log(`Hello your name is ${username} and your age is${age}`);
 }
 
 calculateAge();
+const calculateBirthYear=() =>{
+    let Age= 56;
+    let bornYear = currentYear - Age;
+    console.log(`You were born in ${bornYear}`);
+}
+calculateBirthYear()
