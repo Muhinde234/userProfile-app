700f315068dd6a3d32addc415053facfaabddb2b
ICYEZAGATORE <i.icyezagat1@alustudent.com>
2025-03-25 14:57:20 +0200

    Question 5

--- assets/pages/playground.js
+++ assets/pages/playground.js
@@ -13,3 +13,23 @@ const calculateBirthYear=() =>{
     console.log(`You were born in ${bornYear}`);
 }
 calculateBirthYear()
+
+let personInfo={
+    name:"dosta",
+    age:20,
+    country:"Rwanda",
+   
+}
+console.log(personInfo.age);
+console.log(personInfo.name);
+console.log(personInfo.country);
+//Array destructuring//
+let colors= ["green","red","white"];
+let [first, second] = colors;
+console.log([first, second])
+
+/*let numbers = [10, 20, 30, 40, 50];
+
+let [first, second] = numbers;  // Destructuring the first two elements
+
+console.log([first, second]);  // Output: [10, 20]*/
